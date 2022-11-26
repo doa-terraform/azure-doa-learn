5d24eed200cad7c2609725ad708effb208646ebe
Mohammad Tanvir <tanvir0102@gmail.com>
2022-11-26 15:03:13 +0530

    added myvm resource

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # Resource-1: Azure Resource Group
 resource "azurerm_resource_group" "myrg" {
-  name = "myrg-1"
+  name     = "myrg"
   location = "East US"
 }
 
 # Resource-2: Create Virtual Network
 resource "azurerm_virtual_network" "myvnet" {
-  name                = "myvnet-1"
+  name                = "myvnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
@@ -14,7 +14,7 @@ resource "azurerm_virtual_network" "myvnet" {
 
 # Resource-3: Create Subnet
 resource "azurerm_subnet" "mysubnet" {
-  name                 = "mysubnet-1"
+  name                 = "mysubnet"
   resource_group_name  = azurerm_resource_group.myrg.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
   address_prefixes     = ["10.0.2.0/24"]
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "mysubnet" {
 
 # Resource-4: Create Public IP Address
 resource "azurerm_public_ip" "mypublicip" {
-  name                = "mypublicip-1"
+  name                = "mypublicip"
   resource_group_name = azurerm_resource_group.myrg.name
   location            = azurerm_resource_group.myrg.location
   allocation_method   = "Static"
@@ -32,8 +32,8 @@ resource "azurerm_public_ip" "mypublicip" {
 }
 
 # Resource-5: Create Network Interface
-resource "azurerm_network_interface" "myvm1nic" {
-  name                = "vm1-nic"
+resource "azurerm_network_interface" "myvmnic" {
+  name                = "vmnic"
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
 
@@ -41,6 +41,91 @@ resource "azurerm_network_interface" "myvm1nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.mysubnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.mypublicip.id 
+    public_ip_address_id          = azurerm_public_ip.mypublicip.id
+  }
+}
+
+# Resource-6: Create Network Security Group and rule
+resource "azurerm_network_security_group" "mynsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Resource-7: Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "nsgassoc" {
+  network_interface_id      = azurerm_network_interface.myvmnic.id
+  network_security_group_id = azurerm_network_security_group.mynsg.id
+}
+
+# Resource-8: Generate random text for a unique storage account name
+resource "random_id" "random_id" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.myrg.name
+  }
+
+  byte_length = 8
+}
+
+# Resource-9: Create storage account for boot diagnostics
+resource "azurerm_storage_account" "mystorageaccount" {
+  name                     = "diag${random_id.random_id.hex}"
+  location                 = azurerm_resource_group.myrg.location
+  resource_group_name      = azurerm_resource_group.myrg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Resource-10: Create (and display) an SSH key
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Resource-11: Create virtual machine
+resource "azurerm_linux_virtual_machine" "myvm" {
+  name                  = "myVM"
+  location              = azurerm_resource_group.myrg.location
+  resource_group_name   = azurerm_resource_group.myrg.name
+  network_interface_ids = [azurerm_network_interface.myvmnic.id]
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name                   = "myvm"
+  admin_username                  = "azureuser"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.example_ssh.public_key_openssh
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
   }
 }
